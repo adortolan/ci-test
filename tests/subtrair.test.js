78b3f04a52befd030ccc905d86b42ce2e8832218
adortolan <adortolan@gmail.com>
2023-04-08 16:41:01 -0300

    Arquivos

--- tests/subtrair.test.js
+++ tests/subtrair.test.js
@@ -1,4 +1,4 @@
-const subtrair = require('./subtrair.js') ;
+const subtrair = require('../subtrair') ;
 
 test('subtrair 2 / 2 to equal 1', () => {
     expect(subtrair(2,2)).toBe(1);
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sum.js') ;
+const sum = require('../sum.js') ;
 
 test('Somar 2 + 2 to equal 4', () => {
     expect(sum(2,2)).toBe(4);
